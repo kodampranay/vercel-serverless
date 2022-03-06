8edcfb499ae8544b0d10c88c86a978fc9ed570bb
kodampranay <pranaykodam213@gmail.com>
2022-03-06 18:43:32 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = 5000;
 app.get('/',(req,res) => {
-    res.send("This is a sample express app")
+    res.send({ok:'you are on home page'})
 })
 // Listen on port 5000
 app.listen(port, () => {
