7447a108f8e97a416879915cecbdb17966036db3
kodampranay <pranaykodam213@gmail.com>
2022-03-25 12:49:33 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+require('dotenv').config()
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT;
 app.get('/',(req,res) => {
     res.send({ok:'you are on home page'})
 })
